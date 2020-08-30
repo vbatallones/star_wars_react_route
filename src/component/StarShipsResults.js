68cde6a7f1d8a3b342234fd594230009a8bba9f2
Levin Batallones <67178203+vbatallones@users.noreply.github.com>
2020-08-30 12:26:06 -0700

    [component] fix bug and add a functional return button using useHistory function

--- src/component/StarShipsResults.js
+++ src/component/StarShipsResults.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import {useLocation} from 'react-router'
+import { useLocation, useHistory } from 'react-router';
 
 export default function StarShipsResults() {
     const location = useLocation();
-     console.log(location)
-    return (
-        <div>
-            <h1>Hello</h1>
-            <h4>Name: {location.state.s.name}</h4>
-            <h4>Model: {location.state.s.model}</h4>
-            
-        </div>
-    )
+    const history = useHistory();
+	console.log(location);
+
+    function goBackHandle() {
+        history.goBack()
+    }
+	return (
+		<div>
+			<h1>Hello</h1>
+			<h4>Name: {location.state.s.name}</h4>
+			<h4>Model: {location.state.s.model}</h4>
+            <button onClick={goBackHandle}>Go Back</button>
+		</div>
+	);
 }
